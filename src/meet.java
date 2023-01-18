8d813c23a7a3dffeffed8cb8f945c602f57f9b79
mrguven <mrguven@gmail.com>
2023-01-18 12:54:19 +0100

    update

---
+++ src/meet.java
@@ -0,0 +1,19 @@
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+
+
+public class meet {
+
+
+
+
+
+
+
+
+}
--- src/person.java
+++ src/person.java
@@ -1,3 +1,7 @@
+import com.sun.org.apache.xpath.internal.objects.XString;
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+import java.sql.Array;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
@@ -18,9 +22,15 @@ public class person {
 //
 
 
+        System.out.println(price1());
+
+
     }
 
-    //-------------------
+
+
+///-------------------
+
     public static String kangaroo(int x1, int v1, int x2, int v2) {
         // Write your code here
 
@@ -111,25 +121,94 @@ newScore.add(sec);
 
 
 //---------------
-    public static int migratoryBirds(List<Integer> arr) {
-        // Write your code here
-        ArrayList<Integer> typeIndex = new ArrayList<>();
-        ArrayList<Integer> repeatArray = new ArrayList<>();
-
-        int firstType= 0;
-        int secType = 0;
-        int repeat=0;
+public static int countingValleys(int steps, String path) {
+    // Write your code here
 
 
+int valley = 0;
+int level=0;
+ String[] newPath = new String[path.length()];
+newPath = path.split("");
 
+        for(int  i = 0; i<steps;i++) {
 
-            for (int j=0;i<arr.size();j++) {
-                if (arr.get(i)==arr.get(0)) {
-                        firstType++;
+            if(newPath[i].equals("D")) {
+                level--;
+            }
+             if ( newPath[i].equals("U")){
+                level++;
+            }  if (level==0 && newPath[i].equals("U") ) {
+                valley++;
+            }
 
-               }
         }
-   }
+
+return valley;
+
+
+
+}
+
+
+
+
+    static int getMoneySpent(int[] keyboards, int[] drives, int b) {
+        /*
+         * Write your code here.
+         */
+int totalPrice=0;
+//int SecPrice=0;
+for(int key = 0;key<keyboards.length;key++) {
+    for (int dri=0;dri<drives.length;dri++) {
+       if( totalPrice<keyboards[key]+drives[dri]){
+           if(totalPrice<=b) {
+
+          totalPrice=keyboards[key]+drives[dri];
+       }
+       }
+       }
+  }
+
+        if(totalPrice==0){
+            return -1;
+        }
+        else {
+            return totalPrice;
+        }
+
+    }
+
+
+
+
+
+
+//-----------------------------
+
+
+
+    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
+        // Write your code here
+
+        List<Integer> newScore1= new ArrayList<>();
+
+
+        for(int i=0;i<player.size();i++) {
+            int score= 0;
+            for (int j=0;j<ranked.size();j++) {
+                if (player.get(i)<=ranked.get(j) ) {
+                    score++;
+
+                    if (ranked.get(j)== ranked.get(j-1)) {
+                        score--;
+                    }
+                }
+                newScore.add(score);
+           }
+
+        }
+        System.out.println(newScore1.get(10));
+    }
 
 
 
@@ -138,6 +217,34 @@ newScore.add(sec);
 
 
 
+    //------------------
+
+
+
+
+
+
+public static int price1 () {
+
+    System.out.println("bir tutar giriniz");
+    double km = scan.nextInt();
+
+    double price= 10+ (km *1.20);
+
+    if (price<20) {
+        return "odenecek tutar 20 TL";
+    }
+    else {
+        return price;
+    }
+
+
+}
+
+
+
+
+//---------------------------------
 
 
 
@@ -145,16 +252,47 @@ newScore.add(sec);
 
 
 
+    public static String angryProfessor(int k, List<Integer> a) {
+        // Write your code here
+
+        int count = 0;
+
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) <= 0) {
+                count++;
+            }
+        }
+
+        if (count >= k) {
+            return "NO";
+        } else {
+            return "YES";
+        }
+    }
 
 
 
 
+//-------------------------------
+
+    public static int hurdleRace(int k, List<Integer> height) {
+        // Write your code here
+
+        int doses =-1;
 
 
-
-
-
-
+        for (int i=0;i<height.size();i++) {
+            if (doses < height.get(i)) {
+                doses = height.get(i);
+            }
+        }
+        if (doses > k) {
+            return (doses-k);
+        }
+        else  {
+            return 0;
+        }
+    }
 
 
 
